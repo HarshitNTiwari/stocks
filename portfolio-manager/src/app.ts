d6a3ae3ea48fc6e86059b46045740c481bd9f10e
Harshit Tiwari <20uec059@lnmiit.ac.in>
2024-02-19 22:56:33 +0530

    added middleware for json parsing

--- portfolio-manager/src/app.ts
+++ portfolio-manager/src/app.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+// built-in express middleware for json parsing. 
+// without it you won't be able to parse json data sent in req body
+app.use(express.json({
+    limit: "16kb"
+}))
+
 import watchlistRouter from "./routes/watchlist.routes.ts";
 import holdingRouter from "./routes/holding.routes.ts";
 
