2a92984819d20eed02223b1f9b14013d4fbea429
Harshit Tiwari <20uec059@lnmiit.ac.in>
2024-02-20 22:04:48 +0530

    commit to main

--- api-gateway/src/routers/auth.ts
+++ api-gateway/src/routers/auth.ts
@@ -5,6 +5,7 @@ import { checkLogin } from "../middlewares/index.ts"
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
+// applying checkLogin middleware to check if the user is even logged in!
 router.route("/logout").post(checkLogin, logoutUser);
 
 export default router;
